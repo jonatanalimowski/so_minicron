0fad9637187c7cd0d9482e565a55c773d931be71
jonek <jonatanalimowski@gmail.com>
2026-05-06 17:41:25 +0200

    Ostatnie poprawki

--- tasks.c
+++ tasks.c
@@ -6,8 +6,13 @@
 #include <sys/stat.h>
 #include <signal.h>
 #include <syslog.h>
+#include <fcntl.h>
+#include <sys/wait.h>
 
-typedef struct  
+char *taskfile;
+char *output_filename;
+
+typedef struct Task
 {
 	size_t time;
 	size_t pipe_amount;
@@ -36,7 +41,21 @@ void signal_handler(int signal)
 	}
 }
 
-void free_all(Task* curr) 
+
+void free_task(Task *task) {
+    if (!task) return;
+
+    if (task->prog_names) {
+        for (size_t i = 0; i < (task->pipe_amount + 1); i++) {
+            free(task->prog_names[i]);
+        }
+        free(task->prog_names);
+    }
+
+    free(task);
+}
+
+void free_all(Task* curr)
 {
 	while(curr){
 	Task *help = curr;
@@ -48,50 +67,50 @@ void free_all(Task* curr)
 static void demonize()
 {
 	pid_t pid;
-	
+
 	pid = fork();
 	if (pid < 0)
 	{
 		exit(EXIT_FAILURE);
 	}
-	
+
 	if (pid > 0)
 	{
 		exit(EXIT_SUCCESS);
 	}
-	
+
 	if(setsid() < 0)
 	{
 		exit(EXIT_FAILURE);
 	}
-	
+
 	signal(SIGCHLD, SIG_IGN);
 	signal(SIGHUP, SIG_IGN);
 	signal(SIGINT, signal_handler);
 	signal(SIGUSR1, signal_handler);
 	signal(SIGUSR2, signal_handler);
-	
+
 	pid = fork();
 	if (pid < 0)
 	{
 		exit(EXIT_FAILURE);
 	}
-	
+
 	if (pid > 0)
 	{
 		exit(EXIT_SUCCESS);
 	}
-	
+
 	umask(0);
-	
+
 	chdir("/");
-	
+
 	int x;
 	for(x = sysconf(_SC_OPEN_MAX);x>=0; x--)
 	{
 		close(x);
 	}
-	
+
 	openlog("mini-cron", LOG_PID, LOG_DAEMON);
 }
 
@@ -102,7 +121,7 @@ Task* create_task(int t, int p, const char **cmd, int info) {
 	new_task->info = info;
 	new_task->next = NULL;
     new_task->prog_names = malloc((p+1) * sizeof(char *));
-    
+
     if (new_task->prog_names) {
         for (int i = 0; i < p+1; i++) {
             new_task->prog_names[i] = strdup(cmd[i]);
@@ -112,19 +131,43 @@ Task* create_task(int t, int p, const char **cmd, int info) {
     return new_task;
 }
 
-void free_task(Task *task) {
-    if (!task) return;
-
-    if (Task->prog_names) {
-        for (size_t i = 0; i < (Task->pipe_amount + 1); i++) {
-            free(Task->prog_names[i]);
-        }
-        free(Task->prog_names);
+char** split_command(char *cmd) {
+    int count = 0;
+    char *copy = strdup(cmd);
+    char *token = strtok(copy, " ");
+    while (token) {
+        count++;
+        token = strtok(NULL, " ");
     }
+    free(copy);
 
-    free(Task);
+    char **args = malloc((count + 1) * sizeof(char *));
+    copy = strdup(cmd);
+    token = strtok(copy, " ");
+    for (int i = 0; i < count; i++) {
+        args[i] = strdup(token);
+        token = strtok(NULL, " ");
+    }
+    args[count] = NULL;
+    free(copy);
+    return args;
 }
 
+void insert_sorted(Task **head, Task *new_node) {
+    if (*head == NULL || (*head)->time >= new_node->time) {
+        new_node->next = *head;
+        *head = new_node;
+    } else {
+        Task *current = *head;
+        while (current->next != NULL && current->next->time < new_node->time) {
+            current = current->next;
+        }
+        new_node->next = current->next;
+        current->next = new_node;
+    }
+}
+
+// UZUPELNIC PARSOWANIE O FUNKCJE SPLIT COMMAND
 Task* parse_taskfile(const char *filename) {
     FILE *file = fopen(filename, "r");
     if (!file) return NULL;
@@ -149,21 +192,20 @@ Task* parse_taskfile(const char *filename) {
                 if (full_cmd[i] == '|') pipes++;
             }
 
-            
             char **progs = malloc((pipes + 1) * sizeof(char *));
-            char *command_copy = strdup(full_cmd); 
-            
+            char *command_copy = strdup(full_cmd);
+
             char *single_prog = strtok(command_copy, "|");
             size_t idx = 0;
             while (single_prog != NULL) {
-                
-                while(*single_prog == ' ') single_prog++; 
+
+                while(*single_prog == ' ') single_prog++;
                 progs[idx++] = single_prog;
                 single_prog = strtok(NULL, "|");
             }
 
             Task *new_node = create_task(seconds, pipes, (const char**)progs, info);
-            
+
             if (new_node) {
                 insert_sorted(&head, new_node);
             }
@@ -179,78 +221,111 @@ Task* parse_taskfile(const char *filename) {
 
 
 int main(int argc, char *argv[]) {
-	if(argc!=4){
-	return 1;
-	}
-	taskfile = argv[1];
-	Task curr;
-	while(curr&&running_flag&&read_flag)
-	{
-	read_flag = 0;
-	/*wczytaj, sparsuj i sortując przez wstawianie stwórz stos FIFO ustawiając Task* curr i Task* next */
-	curr = parse_taskfile(taskfile);
-	while(curr&&running_flag)
-	{
-		sleep(curr->time);
-		/*utwórz n procesów i połącz je pipe()*/
-		pid_t pid = fork();
-
-		if (pid < 0) {	
-		perror("fork");
+	if(argc!=3){
 		return 1;
-		} } else if (pid == 0) {
-    
-    int num_progs = curr->pipe_amount + 1;
-    int pipefds[2 * (num_progs - 1)];
-
-    for (int i = 0; i < num_progs - 1; i++) {
-        if (pipe(pipefds + i * 2) < 0) {
-            perror("pipe");
-            _exit(EXIT_FAILURE);
-        }
-    }
-
-    for (int i = 0; i < num_progs; i++) {
-        pid_t p_pid = fork();
-        if (p_pid == 0) {
-            if (i > 0) {
-                dup2(pipefds[(i - 1) * 2], STDIN_FILENO);
-            }
-            
-            if (i < num_progs - 1) {
-                dup2(pipefds[i * 2 + 1], STDOUT_FILENO);
-            } else {
-                int fd = open(output_filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
-                if (fd != -1) {
-                    dup2(fd, STDOUT_FILENO);
-                    close(fd);
-                }
-            }
-
-            for (int j = 0; j < 2 * (num_progs - 1); j++) {
-                close(pipefds[j]);
-            }
-
-            char *args[] = {curr->prog_names[i], NULL};
-            execvp(args[0], args);
-            perror("execvp");
-            _exit(EXIT_FAILURE);
-        }
-    }
-
-    for (int i = 0; i < 2 * (num_progs - 1); i++) {
-        close(pipefds[i]);
-    }
-    for (int i = 0; i < num_progs; i++) {
-        wait(NULL);
-    }
-    _exit(EXIT_SUCCESS);
-}
-		Task* help = curr;
-		curr = curr->next;
-		free_task(help);
 	}
-	free_all(curr);
+
+	taskfile = argv[1];
+	output_filename = argv[2];
+	demonize();
+	Task *curr = NULL;
+
+	while(running_flag)
+	{
+		// Obsługa SIGUSR1
+		if (read_flag) {
+			if (curr) free_all(curr);
+			curr = parse_taskfile(taskfile);
+			read_flag = 0;
+			syslog(LOG_INFO, "Wczytano zadania ponownie.");
+		}
+
+		// Obsługa SIGUSR2
+		if (log_flag) {
+			Task *help = curr;
+			while (help) {
+				syslog(LOG_INFO, "Zadanie w kolejce: info=%d", help->info);
+				help = help->next;
+			}
+			log_flag = 0;
+		}
+
+		// Wykonywanie Zadan
+		if (curr) {
+			time_t now = time(NULL);
+			struct tm *t = localtime(&now);
+			size_t seconds_now = t->tm_hour * 3600 + t->tm_min * 60 + t->tm_sec;
+			int diff = (int)curr->time - (int)seconds_now;
+
+			if (diff > 0) {
+				sleep(diff);
+			} else if (diff < 0) {
+				diff +=  86400;
+				sleep(diff);
+			}
+
+			pid_t pid = fork();
+			if (pid < 0) {
+				perror("fork");
+				return 1;
+			}
+
+			else if (pid == 0) {
+		    		int num_progs = curr->pipe_amount + 1;
+		    		int pipefds[2 * (num_progs - 1)];
+
+				for (int i = 0; i < num_progs - 1; i++) {
+		        		if (pipe(pipefds + i * 2) < 0) {
+		            			perror("pipe");
+		            			_exit(EXIT_FAILURE);
+		        		}
+		    		}
+
+		    		for (int i = 0; i < num_progs; i++) {
+		        		pid_t p_pid = fork();
+					//TUTAJ JAKIES POPRAWKI
+		        		if (p_pid == 0) {
+						// łączenie stdin z stdout w potoku
+		            			if (i > 0) dup2(pipefds[(i - 1) * 2], STDIN_FILENO);
+		            			if (i < num_progs - 1) dup2(pipefds[i * 2 + 1], STDOUT_FILENO);
+		            			else {
+							// ostatnie polecenie w potoku
+		                			int fd = open(output_filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
+					                if (fd != -1) {
+								dprintf(fd, "Polecenie: %s \n", curr->prog_names[i]);
+					                	if (curr->info == 0 || curr->info == 2) dup2(fd, STDOUT_FILENO);
+					                	if (curr->info == 1 || curr->info == 2) dup2(fd, STDERR_FILENO);
+								close(fd);
+					                }
+			      			}
+
+						// Zamkniecie deskryptorow w dziecku
+					        for (int j = 0; j < 2 * (num_progs - 1); j++) {
+					        	close(pipefds[j]);
+					        }
+
+					        char **args = split_command(curr->prog_names[i]);
+					        execvp(args[0], args);
+					        perror("execvp failed");
+					        _exit(EXIT_FAILURE);
+					}
+				}
+
+				for (int j = 0; j < 2 * (num_progs - 1); j++) close(pipefds[j]);
+				for (int i = 0; i < num_progs; i++) wait(NULL);
+				syslog(LOG_INFO, "Zadanie zakończone.");
+				_exit(EXIT_SUCCESS);
+
+			} else {
+				wait(NULL);
+				Task* help = curr;
+				curr = curr->next;
+				free_task(help);
+			}
+		}
 	}
+
+	if (curr) free_all(curr);
+	closelog();
 	return 0;
 }
